536504fb07384212eaf163d4d4504c301bc0a751
iconicJose <josecr1304@gmail.com>
2025-11-12 00:35:15 -0600

    init

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+// This file will hold all the interactive behavior for your photo site.
+// Right now it is empty on purpose.
+// We are creating the structure first, then we will add features step by step.
